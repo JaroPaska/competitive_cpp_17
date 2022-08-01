96fe5e2869b91e0de883061c61b875ce27a05c7d
Jaroslav Paska <jarko.paska@gmail.com>
2022-08-01 02:20:39 +0200

    idk

--- polynomial_hash.h
+++ polynomial_hash.h
@@ -4,7 +4,30 @@
 
 #pragma once
 
-#include "base.h"
+#include "number_theory.h"
+
+class Hash {
+public:
+    constexpr Hash(long long value, int size) : value_(value), size_(size) {}
+
+    [[nodiscard]] constexpr Hash extend(const Hash& rhs) const {
+        return Hash(mod(mod_mul(value_, power(rhs.size_), M) + rhs.value_, M), size_ + rhs.size_);
+    }
+
+    static constexpr long long P = 269;
+    static constexpr long long M = 1e18 + 9;
+
+private:
+    static long long power(int exponent) {
+        static std::vector<long long> powers{1};
+        while (powers.size() <= exponent)
+            powers.emplace_back(P * powers.back() % M);
+        return powers[exponent];
+    }
+
+    long long value_ = 0;
+    int size_ = 0;
+};
 
 template<long long P, long long M>
 struct PolynomialHash {
